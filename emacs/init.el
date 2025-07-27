c5c7758c619c916aed9549eb0de40982d749218d
Kamil2000 <kamiladamski96@gmail.com>
2025-07-27 12:29:16 +0200

    emacs additions

--- emacs/init.el
+++ emacs/init.el
@@ -21,18 +21,38 @@
 (setq-default tab-width 4)
 (setq c-set-style "k&r")
 (setq c-basic-offset 4)
+
 ; ==========================================
 
 (require 'package)
-(setq package-check-signature 'allow-unsigned) ; nil or 'allow-unsigned
 
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(setq package-archive-priorities '(("gnu" . 20)("melpa-stable" . 10)))
+; (setq package-archive-priorities '(("gnu" . 20)("melpa-stable" . 10)))
+(setq package-enable-at-startup t)
 
-; package-selected-packages to see packages
-; neotree, gnu-elpa-keyring-update (as quickfix)
-; (package-initialize t)
-; (setq package-enable-at-startup nil) ; necessery when above line uncommented
+(if (package-installed-p 'gnu-elpa-keyring-update)
+  (setq package-check-signature 'allow-unsigned)
+  (setq package-check-signature nil))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(setq package-selected-packages nil)
+
+; BEGIN list of packages
+(add-to-list 'package-selected-packages 'gnu-elpa-keyring-update t)
+(add-to-list 'package-selected-packages 'treemacs t)
+(add-to-list 'package-selected-packages 'lsp-mode t)
+(add-to-list 'package-selected-packages 'lsp-ui t)
+(add-to-list 'package-selected-packages 'company t)
+(add-to-list 'package-selected-packages 'dap-mode t)
+; END list of packages
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
 
 ; ===========================================
+
